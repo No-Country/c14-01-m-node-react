20c2fb2882c7c23fcb5490b6a62b454a32b0c944
veronicajujuy <veronica.jujuy@gmail.com>
2023-10-10 17:51:26 -0300

    Navbar terminada sin search funcionando

---
+++ frontend/public/icons/icons.js
@@ -0,0 +1,31 @@
+const icons = [
+  {
+    tropical:
+      "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
+  },
+  {
+    trending:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+  },
+  {
+    island:
+      "https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg",
+  },
+  {
+    farms:
+      "https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
+  },
+  {
+    camping:
+      "https://a0.muscache.com/pictures/ca25c7f3-0d1f-432b-9efa-b9f5dc6d8770.jpg",
+  },
+  {
+    iconic_cities:
+      "https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg",
+  },
+  {
+    lake: "https://a0.muscache.com/pictures/a4634ca6-1407-4864-ab97-6e141967d782.jpg",
+  },
+];
+
+export default icons;
---
+++ frontend/src/components/Header/DropdownMenu.jsx
@@ -0,0 +1,27 @@
+import Dropdown from "react-bootstrap/Dropdown";
+import "./styles.css";
+
+// eslint-disable-next-line react/prop-types
+function DropDownMenu({ children }) {
+  return (
+    <Dropdown id="dropdown-basic-button">
+      <Dropdown.Toggle variant={null} id="dropdown-basic">
+        {children}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Messages</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Notifications</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Trips</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Wish Lists</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item href="#/action-3">AirBnB your home</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Account</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item href="#/action-3">AirBnB your home</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Account</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default DropDownMenu;
