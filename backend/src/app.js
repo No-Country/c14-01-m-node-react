cc4ed15af5b054e28f8237730ecbcbbaa63801ec
MartinBerdun <mberdun.98@gmail.com>
2023-10-10 15:18:28 -0300

    Server initialized and dependencies installed

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const httpServer = app.listen( 8080 , () => {
+    console.log("Listening on port 8080");
+  });
+
+  
