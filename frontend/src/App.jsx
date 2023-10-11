240e0d9bc4b21b4365465c493ca3f45b6768ed0c
veronicajujuy <veronica.jujuy@gmail.com>
2023-10-11 01:12:28 -0300

    arreglando estilos de categorias

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Categories from "./components/CategoriesNav/Categories";
 import Navbar from "./components/NavBar/Navbar";
+import ListCategories from "./components/Filters/ListCategories";
+import ListPropertys from "./components/List/ListPropertys";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Categories />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Navbar />}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Categories />
+                  <ListPropertys />
+                </>
+              }
+            />
+
+            <Route path="/filters/:category" element={<ListCategories />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ frontend/src/components/List/ListPropertys.jsx
@@ -0,0 +1,7 @@
+import locations from "../../data/locations.json";
+const ListPropertys = () => {
+  console.log(locations);
+  return <div>hello</div>;
+};
+
+export default ListPropertys;
