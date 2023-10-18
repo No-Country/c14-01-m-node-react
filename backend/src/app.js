3a92f1411aceef3c4a3862de596379a5e7068adf
Jose martin Miño <98369061+martin241125@users.noreply.github.com>
2023-10-17 22:48:43 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import database from '../database/database.js'
 import { userRouter } from '../routes/users.js';
+import { placeRouter } from '../routes/places.js';
 
 const app = express();
 app.disable('x-powered-by')
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT ?? 8080
 
 app.use('/users', userRouter)
+app.use('/places' , placeRouter)
 
 app.listen(PORT , () => {
     console.log(`server listening on port http://localhost:${PORT}`);
