2db98ec023ff60fac2f17504527691bae5e82986
MorardRamiro <rramiromorard@gmail.com>
2023-10-20 10:34:09 -0300

    add token to local storage, birthday

--- frontend/src/redux/reducers/authReducer.js
+++ frontend/src/redux/reducers/authReducer.js
@@ -2,7 +2,7 @@ import { SIGN_UP, LOG_IN, LOG_OUT } from '../actions/types';
 
 const initialState = {
   isAuthenticated: false,
-  user: { status: null, message: null },
+  user: { status: null, message: null, token: null },
 };
 
 export default function authReducer(state = initialState, action) {
@@ -20,10 +20,14 @@ export default function authReducer(state = initialState, action) {
     case LOG_OUT:
       return {
         isAuthenticated: false,
-        user: { status: null, message: null },
+        user: { status: null, message: null, token: null },
       }
     default: {
       return state;
     }
   }
+};
+
+export const saveTokenToLocalStorage = (token) => {
+  localStorage.setItem('auth_token', JSON.stringify(token))
 };
