2c7925dad5ae473355a59702a59f76f0eabcf663
marcelogere <marcelogereniere@gmail.com>
2023-10-23 16:58:19 -0300

    Update e2e and package

--- cypress/e2e/api-user.cy.js
+++ cypress/e2e/api-user.cy.js
@@ -1,21 +1,15 @@
-import userModel from "../../backend/models/userModel";
-import { userRouter } from "../../backend/routes/users";
-import { usersByEmail } from "../../backend/controllers/user.controllers";
-import { registerUser } from "../../backend/controllers/authControllers";
+// cypress/integration/user.spec.js
 
+describe('Buscar un usuario por correo electrónico', () => {
+  it('debería encontrar un usuario existente en la base de datos', () => {
+    // Reemplaza ":email" con un correo real en la base de datos
+    const emailToSearch = 'marcelogereniere@hotmail.com';
 
-
-describe("Users API", function () {
-  it("get a user profile by username", function () {
-    const user = { email };
-    cy.request("GET", `/api/users/${email}`).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.user).to.deep.equal({
-        firstName: firstName,
-        lastName: lastName,
-        avatar: avatar,
+    cy.request('GET', `http://localhost:5173/api/users/email/${emailToSearch}`)
+      .then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body.message).to.equal('Usuario encontrado');
+        expect(response.body.user.email).to.equal(emailToSearch);
       });
-      expect(response.body.user).not.to.have.property("balance");
-    });
   });
-})
+});
--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -10,7 +10,7 @@ describe('Auth Module', () => {
     password: "12345678",
   }
   beforeEach(() => {
-    cy.visit('http://localhost:5173/')
+    cy.visit('http://localhost:5173')
   })
   it.only('TC003: Verificar el correcto registro de usuarios, con todos los datos validos', () => {
     
@@ -26,19 +26,6 @@ describe('Auth Module', () => {
     cy.get('input[name="password"]').type(userInfo.password)
     cy.get('.d-grid > .btn').contains("Agree and continue").should('be.visible').click()
     cy.wait(1000)
-    cy.request({
-      method: 'GET',
-      url: 'http://localhost:5173/api/users',
-      body: {
-        email: "user",
-    }
-    }).then((response) => {
-      // Verificar que la solicitud haya tenido éxito (código de respuesta 200)
-      expect(response.status).to.eq(200);
-      
-      // Verificar que el usuario se haya encontrado en la respuesta
-      expect(response.body.user.email).to.eq(userInfo.email);
-    });
     
   })
   it('TC008: Verificar el correcto inicio de sesion con todos los daos validos', () => {
